f06647b24d159d34287eadb4039e2f629375f8f9
Gun Park <kun1998@naver.com>
2023-06-04 20:46:01 +0900

    샘플씬 세팅

--- Assets/02_Scripts/Spaceship/Spaceship.cs
+++ Assets/02_Scripts/Spaceship/Spaceship.cs
@@ -8,20 +8,36 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Spaceship : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 2000f;
     [SerializeField] private float rollAmount;
     [SerializeField] private float pitchAmount;
     [SerializeField] private float yawAmount;
     [SerializeField] private float lerpAmount;
+
+    public List<GameObject> boosterObjects;
+
+    private List<Vector3> boosterOriginalLocalScaleList = new();
+    //private List<Vector3>
 
     private Vector3 rotateValue;
     private Vector2 inputVector;
 
     private Rigidbody rigid;
 
+    private void Awake()
+    {
+
+    }
+
     private void Start()
     {
         rigid = GetComponent<Rigidbody>();
+
+        foreach (GameObject go in boosterObjects)
+        {
+            Vector3 originalScale = go.transform.localScale;
+            boosterOriginalLocalScaleList.Add(originalScale);
+        }
     }
 
     private void FixedUpdate()
@@ -44,5 +60,31 @@ public class Spaceship : MonoBehaviour
     public void GetRotateVector(Vector2 inputVector)
     {
         this.inputVector = inputVector;
+    }
+
+    public void Throttle(float strength)
+    {
+        float inputStrength = (strength - 50f) / 50f;
+
+        if (inputStrength == 0)
+            return;
+
+        speed += inputStrength * 500 * Time.deltaTime;
+        if(speed < 0)
+        {
+            speed = 0f;
+        }
+        else if(speed > 3000f)
+        {
+            speed = 3000f;
+        }
+
+        //속도 2000이 기본값 : 1
+        float boosterScale = speed / 2000f;
+        foreach(GameObject go in boosterObjects)
+        {
+            Vector3 originalScale = boosterOriginalLocalScaleList[boosterObjects.IndexOf(go)];
+            go.transform.localScale = originalScale * boosterScale;
+        }
     }
 }

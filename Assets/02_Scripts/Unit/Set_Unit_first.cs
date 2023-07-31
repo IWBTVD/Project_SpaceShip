032077d7f62edfd0680ff0d55d1936a0584950f4
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-07-31 18:35:40 +0900

    Collision mask

--- Assets/02_Scripts/Unit/Set_Unit_first.cs
+++ Assets/02_Scripts/Unit/Set_Unit_first.cs
@@ -7,4 +7,11 @@ public class Set_Unit_first : MonoBehaviour
     bool isUnit = false;
 
 
+
+    private void OnCollisionEnter(Collision other) {
+       if(other.gameObject.layer == LayerMask.NameToLayer("ShipExterior"))
+        {
+            print("1");
+        }
+    }
 }

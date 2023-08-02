8e9735ead8668eb75fcecfb25e63bfb236498314
Digiforet_Gun_Park <101655523+SweetFlush@users.noreply.github.com>
2023-08-02 11:48:41 +0900

    startButton.cs 소량 수정

--- Assets/02_Scripts/GameSystem/StartButton.cs
+++ Assets/02_Scripts/GameSystem/StartButton.cs
@@ -1,9 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StartButton : MonoBehaviour
 {
+    [SerializeField] Oculus.Interaction.InteractableUnityEventWrapper eventWrapper;
+
+    public void OnEnable()
+    {
+        //이벤트 구독
+        eventWrapper.WhenSelect.AddListener(StartGame);
+    }
+
+    public void OnDisable()
+    {
+        //disable될 때 이벤트 구독 해제
+        eventWrapper.WhenSelect.RemoveListener(StartGame);
+    }
+
+    public void OnDestroy()
+    {
+        //destroy될 때 이벤트 구독 해제
+        eventWrapper.WhenSelect.RemoveListener(StartGame);
+    }
+
     public void StartGame(){
         WorldBoardManager.Instance.NextStage();
         Destroy(gameObject);
